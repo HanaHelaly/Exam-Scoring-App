07f8a6566a8c03f8c71f5bd7bc167a245ac216bc
HanaHelaly <hanahelaly99@gmail.com>
2024-07-16 06:26:53 +0300

    requirements.txt adjustment

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ import time
 import os
 import re
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-from langchain_community.vectorstores import Chroma
 from langchain_community.vectorstores import FAISS
 from langchain_huggingface import HuggingFaceEmbeddings
 from langchain.load import dumps, loads
